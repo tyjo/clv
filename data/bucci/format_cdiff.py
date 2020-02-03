a2cbc80a725e41acb36a1ec30bf48aa143fad6d7
tjoseph <tjoseph@cs.columbia.edu>
2020-02-03 13:54:07 -0500

    updated data

--- data/bucci/format_cdiff.py
+++ data/bucci/format_cdiff.py
@@ -1,5 +1,57 @@
 import numpy as np
 import pickle as pkl
+import os
+
+
+def plot_trajectories(Y, T, output_dir, outfile):
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+
+    def plot_bar(ax, y, time, unique_color_id, remaining_ids):
+        T = y.shape[0]
+        cm = plt.get_cmap("tab20c")
+        colors = [cm(i) for i in range(20)]
+        #time = np.array([t for t in range(T)])
+        widths = np.concatenate((time[1:] - time[:-1], [1])).astype(float)
+        widths[widths > 1] = 1
+
+        widths -= 1e-1
+
+        y_colors = y[:,unique_color_id]
+        ax.bar(time, y_colors[:,0], width=widths, color=colors[0], align="edge")
+        for j in range(1, y_colors.shape[1]):
+            ax.bar(time, y_colors[:,j], bottom=y_colors[:,:j].sum(axis=1), width=widths, color=colors[j], align="edge")
+        
+        ax.bar(time, y[:,remaining_ids].sum(axis=1), bottom=y_colors.sum(axis=1), width=widths, color=colors[19], align="edge")
+        #ax.set_title("Relative Abundances", fontsize=10)
+        #ax.legend(prop={"size" : 4}, bbox_to_anchor=[-0.1,1.225], loc="upper left", ncol=4)
+
+    def find_top_ids(Y, n):
+        ntaxa = Y[0].shape[1]
+        rel_abun = np.zeros(ntaxa)
+        for y in Y:
+            tpts = y.shape[0]
+            denom = y.sum(axis=1,keepdims=True)
+            denom[denom == 0] = 1
+            p = y / denom
+            rel_abun += p.sum(axis=0) / tpts
+        ids = np.argsort(-rel_abun)
+        return np.sort(ids[:n]), np.sort(ids[n:])
+
+    N = len(Y)
+    top19_ids, remaining_ids = find_top_ids(Y, 19)
+    fig, ax = plt.subplots(nrows=N,ncols=1,figsize=(N,2*N))
+    for i in range(N):
+        denom = Y[i].sum(axis=1)
+        denom[denom == 0] = 1
+        plot_bar(ax[i], (Y[i].T / denom).T, T[i], top19_ids, remaining_ids)
+
+
+    outfile = os.path.splitext(outfile)[0]
+    plt.tight_layout()
+    plt.savefig(output_dir + "/" + outfile + ".pdf")
+    plt.close()
+
 
 sample_id_to_subject_id = {}
 subject_id_time = {}
@@ -7,16 +59,15 @@ subject_id_u = {}
 subject_id_biomass = {}
 
 f = open("data_cdiff/metadata.txt", "r")
-g = open("data_cdiff/biomass.txt", "r")
+#g = open("data_cdiff/biomass.txt", "r")
 
-for line1, line2 in zip(f, g):
-    line1 = line1.split()
-    if "sampleID" in line1[0]:
+for line in f:
+    line = line.split()
+    if "sampleID" in line[0]:
         continue
-    sample_id = line1[0]
-    subject_id = line1[2]
-    day = float(line1[3])
-    #perturb = int(line1[4])
+    sample_id = line[0]
+    subject_id = int(line[2])
+    day = float(line[3])
     
     if day == 28.75:
         perturb = 1
@@ -27,33 +78,16 @@ for line1, line2 in zip(f, g):
     subject_id_time[subject_id] = subject_id_time.get(subject_id, []) + [day]
     subject_id_u[subject_id] = subject_id_u.get(subject_id, []) + [perturb]
 
-    line2 = line2.split()
-    line2 = [float(mass) for mass in line2]
-    subject_id_biomass[subject_id] = subject_id_biomass.get(subject_id, []) + [np.mean(line2)]
 f.close()
-g.close()
 
-counts = np.loadtxt("data_cdiff/counts.txt", delimiter="\t", dtype=str, comments="!")
-# remove taxa where counts are mostly 0/1
-remove_idx = [1, 15, 17, 20, 21, 23]
-keep_idx = [ i for i in range(24) if i not in remove_idx ]
-remove = counts[remove_idx,1:].astype(float)
-counts = counts[keep_idx,1:]
-#counts = np.vstack((counts, remainder))
-#print(remainder)
-
+denoised_data_files = ["denoise-conc-1.csv", "denoise-conc-2.csv", 
+                        "denoise-conc-3.csv", "denoise-conc-4.csv",
+                        "denoise-conc-5.csv"]
 
 subject_id_counts = {}
-
-for row in counts.T:
-    sample_id = row[0]
-    counts = row[1:].astype(float)
-    subject_id = sample_id_to_subject_id[sample_id]
-
-    if subject_id in subject_id_counts:
-        subject_id_counts[subject_id] = np.vstack( (subject_id_counts[subject_id], np.array(counts)) )
-    else:
-        subject_id_counts[subject_id] = np.array(counts)
+for i,f in enumerate(denoised_data_files):
+    matrix = np.loadtxt("data_cdiff/" + f, encoding='utf-8-sig', delimiter=",").T
+    subject_id_counts[i+1] = matrix
 
 Y_cdiff = []
 U_cdiff = []
@@ -66,20 +100,13 @@ for subject_id in subject_id_counts:
     u = np.array(subject_id_u[subject_id])
     u = u.reshape((u.size, 1))
 
-    mass = np.array(subject_id_biomass[subject_id])
-    y_mass = y / y.sum(axis=1,keepdims=True)
-    y_mass = (y_mass.T * mass/1e9).T
-    zero_count += y_mass[y_mass == 0].size
-    total_count += y_mass.size
-    
-    Y_cdiff.append(y_mass)
+    Y_cdiff.append(y)
     U_cdiff.append(u)
     T_cdiff.append(t)
 
-pkl.dump(Y_cdiff, open("Y_cdiff.pkl", "wb"))
+#plot_trajectories(Y_cdiff, T_cdiff, "./", "cdiff-denoised")
+pkl.dump(Y_cdiff, open("Y_cdiff-denoised.pkl", "wb"))
 pkl.dump(U_cdiff, open("U_cdiff.pkl", "wb"))
 pkl.dump(T_cdiff, open("T_cdiff.pkl", "wb"))
-
-print("% 0", zero_count / total_count)
 
 
--- data/bucci/format_diet.py
+++ data/bucci/format_diet.py
@@ -1,9 +1,79 @@
 import numpy as np
 import pickle as pkl
 
+from scipy.interpolate import UnivariateSpline
+
+from format_cdiff import plot_trajectories
+
 sample_id_to_subject_id = {}
 subject_id_time = {}
 subject_id_u = {}
+
+
+def compute_breakpoints(effects):
+    """Break the spline when external perturbations occur."""
+    breakpoints = []
+    for u in effects:
+        in_perturb = False
+        v = []
+        for i,ut in enumerate(u):
+            if i == 0 or i == u.shape[0]-1:
+                v.append(1)
+                continue
+            if np.any(ut) > 0 and not in_perturb:
+                v.append(1)
+                in_perturb = True
+            elif np.any(ut) > 0 and in_perturb:
+                v.append(0)
+            elif np.all(ut) == 0 and in_perturb:
+                i = 1 if v[i-1] == 0 else 0
+                v.append(i)
+                in_perturb = False
+            else:
+                v.append(0)
+        v = np.array(v)
+        breakpoints.append(np.nonzero(v)[0])
+    return breakpoints
+
+
+def denoise(counts, t_pts, effects=None):
+    """Takes a sequence of counts at t_pts, and returns denoised estimates
+    of latent trajectories."""
+    ntaxa = counts[0].shape[1]
+    denoised_traj = []
+
+    if effects is not None:
+        breakpoints = compute_breakpoints(effects)
+        for c,t,b in zip(counts,t_pts,breakpoints):
+            denoised = np.zeros(c.shape)
+            mass = c.sum(axis=1,keepdims=True)
+            p = c / c.sum(axis=1,keepdims=True)
+            p[p==0] = 1e-5
+            p /= p.sum(axis=1,keepdims=True)
+            c = (mass.T*p.T).T
+            for i in range(ntaxa):
+                for j in range(1,b.size):
+                    start = b[j-1]
+                    end = b[j]+1
+                    k = 5 if end - start <= 3 else 5
+                    #k = 3
+                    f = UnivariateSpline(t[start:end],c[start:end,i],k=k)
+                    denoised[start:end,i] = f(t[start:end])
+            denoised[0] = c[0]
+            denoised = np.clip(denoised, np.min(denoised[denoised > 0]), np.inf)
+            denoised_traj.append(denoised)
+    else:
+        for c,t in zip(counts,t_pts):
+            denoised = np.zeros(c.shape)
+            k = 3 if t.shape[0] <= 5 else 5
+            for i in range(ntaxa):
+                f = UnivariateSpline(t,c[:,i],k=k)
+                denoised[:,i] = f(t)
+            denoised = np.clip(denoised, np.min(denoised[denoised > 0]), np.inf)
+            denoised /= denoised.sum(axis=1,keepdims=True)
+            denoised_traj.append(denoised)
+
+    return denoised_traj
 
 
 with open("data_diet/metadata.txt", "r") as f:
@@ -50,7 +120,7 @@ U_diet = []
 T_diet = []
 zero_counts = 0
 total_counts = 0
-for subject_id in subject_id_counts:
+for subject_id in sorted(subject_id_counts):
     y = np.array(subject_id_counts[subject_id])
     t = np.array(subject_id_time[subject_id])
     u = np.array(subject_id_u[subject_id])
@@ -62,9 +132,13 @@ for subject_id in subject_id_counts:
     U_diet.append(u)
     T_diet.append(t)
 
-    print(y.shape)
+
+Y_diet_denoised = denoise(Y_diet, T_diet, effects=U_diet)
+#plot_trajectories(Y_diet_denoised, T_diet, "./", "diet-denoised")
+#plot_trajectories(Y_diet, T_diet, "./", "diet-raw")
 
 pkl.dump(Y_diet, open("Y_diet.pkl", "wb"))
+pkl.dump(Y_diet_denoised, open("Y_diet_denoised.pkl", "wb"))
 pkl.dump(U_diet, open("U_diet.pkl", "wb"))
 pkl.dump(T_diet, open("T_diet.pkl", "wb"))
 
